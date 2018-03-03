ee0212c2d851ea67c4b9dae53ee0a9c659b6dd16
shiguangchicken <31305323+shiguangchicken@users.noreply.github.com>
2018-03-03 13:59:02 +0800

    微信小程序登录

--- test.php
+++ test.php
@@ -1,66 +1,21 @@
 <?php
-//确保在连接客户端时不会超时
-set_time_limit(0);
-
-$ip = '127.0.0.1';
-$port = 1935;
-
-/*
- +-------------------------------
- *    @socket通信整个过程
- +-------------------------------
- *    @socket_create
- *    @socket_bind
- *    @socket_listen
- *    @socket_accept
- *    @socket_read
- *    @socket_write
- *    @socket_close
- +--------------------------------
- */
-
-/*----------------    以下操作都是手册上的    -------------------*/
-if(($sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
-    echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
-}
-
-if(($ret = socket_bind($sock,$ip,$port)) < 0) {
-    echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
-}
-
-if(($ret = socket_listen($sock,4)) < 0) {
-    echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
-}
-
-$count = 0;
-
-do {
-    if (($msgsock = socket_accept($sock)) < 0) {
-        echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
-        break;
-    } else {
-
-        //发到客户端
-        $msg ="测试成功！\n";
-        socket_write($msgsock, $msg, strlen($msg));
-
-        echo "测试成功了啊\n";
-        $buf = socket_read($msgsock,8192);
-
-
-        $talkback = "收到的信息:$buf\n";
-        echo $talkback;
-
-        if(++$count > 3){
-            break;
-        };
-
-
+//微信小登录
+ class ToDataBase{
+    public function connection($host='localhost',$uer='root',$pw='',$database='test'){
+       $db= mysqli_connect($host,$uer,$pw,$database) or die("数据库连接失败");
+       return $db;
     }
-    //echo $buf;
-    socket_close($msgsock);
 
-} while (true);
-
-socket_close($sock);
+}
+//获取post信息
+$c_number=$_GET['c_number'];
+ $password=$_GET['password'];
+$DataBase=new ToDataBase();
+ $db=$DataBase->connection();
+$sq="select * from lab_member where cnumber='".$c_number."'";
+ $reslt=mysqli_query($db,$sq);
+ $res_row=mysqli_fetch_row($reslt);
+ if ($res_row[2]==$password)
+     echo "ok";
+ else echo "fail"
 ?>
